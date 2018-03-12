7ac434e6867f5c853d0f749b40dcaa408e542775
msunardi <misunardi@hotmail.com>
2018-03-11 21:21:14 -0700

    Implement full GRU cell
    still has bugs - try test(2,1)

--- gru.py
+++ gru.py
@@ -12,7 +12,8 @@ class RN(object):
         self.Whrh = self.random(fan_out, fan_out)
         self.Whx = self.random(fan_in, fan_out)
         self.bh = self.random()
-        self.Wz = self.random(fan_out, fan_out)
+        self.Wzx = self.random(fan_out, fan_out)
+        self.Wzh = self.random(fan_out, fan_out)
         self.bz = self.random()
         self.ht_1 = np.zeros((fan_out,))
         
@@ -20,12 +21,16 @@ class RN(object):
         if mode==0:
             return np.random.mtrand._rand.rand(args[0], args[1])
         else:
-            return np.random.uniform(-2.0, 2.0, args)
+            return np.random.uniform(-1.0, 1.0, args)
         
     def activate(self, x):
         x = np.array(x, dtype=np.float64)
         rt = self.sigmoid(np.dot(x, self.Wrx) + np.dot(self.ht_1, self.Wrh) + self.br)
-        return rt
+        zt = self.sigmoid(np.dot(x, self.Wzx) + np.dot(self.ht_1, self.Wzh) + self.bz)
+        h_t = np.tanh(np.dot(rt*self.ht_1, self.Whrh) + np.dot(x, self.Whx) + self.bh)
+        ht = (1 - zt) * self.ht_1 + zt * h_t
+        self.ht_1 = ht
+        return ht
             
     # Source: https://gist.github.com/jovianlin/805189d4b19332f8b8a79bbd07e3f598
     def sigmoid(self, x, derivative=False):
